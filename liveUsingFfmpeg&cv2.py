39f5ba151e4f9cc5ccd37f227695461b7feb1158
Sourab Maity <62734958+sourabmaity@users.noreply.github.com>
2023-05-04 12:44:12 +0530

    Add files via upload

---
+++ liveUsingFfmpeg&cv2.py
@@ -0,0 +1,44 @@
+import subprocess
+import cv2
+
+# Change this line
+rtmp_url = 'rtmp://testlive-testmediaservices-inwe.channel.media.azure.net:1935/live/9ee531e6dfd74b0eb3391f0486912484/testlive'
+
+# In my mac webcamera is 0, also you can set a video file name instead, for example "/home/user/demo.mp4"
+path = 0
+cap = cv2.VideoCapture(path)
+
+# gather video info to ffmpeg
+fps = int(cap.get(cv2.CAP_PROP_FPS))
+width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+# command and params for ffmpeg
+command = ['ffmpeg',
+           '-y',
+           '-f', 'rawvideo',
+           '-vcodec', 'rawvideo',
+           '-pix_fmt', 'bgr24',
+           '-s', "{}x{}".format(width, height),
+           '-r', str(fps),
+           '-i', '-',
+           '-c:v', 'libx264',
+           '-pix_fmt', 'yuv420p',
+           '-preset', 'ultrafast',
+           '-f', 'flv',
+           rtmp_url]
+
+# using subprocess and pipe to fetch frame data
+p = subprocess.Popen(command, stdin=subprocess.PIPE)
+
+
+while cap.isOpened():
+    ret, frame = cap.read()
+    if not ret:
+        print("frame read failed")
+        break
+
+    # YOUR CODE FOR PROCESSING FRAME HERE
+
+    # write to pipe
+    p.stdin.write(frame.tobytes())
